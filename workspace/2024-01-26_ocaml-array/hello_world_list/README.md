0009e51c7575f9f341bf4806defb61429ff1d5b8
OKAZAKI Shogo <okazaki@zakioka.net>
2024-01-26 23:30:22 +0900

    リストでHello,World!(ocaml)

---
+++ workspace/2024-01-26_ocaml-array/hello_world_list/README.md
@@ -0,0 +1,7 @@
+# OCaml で Hello, World! を出力する（リスト版）
+## How to Run
+
+```
+opam exec -- dune build
+opam exec -- dune exec hello_world_list
+```
---
+++ workspace/2024-01-26_ocaml-array/hello_world_list/bin/main.ml
@@ -0,0 +1,4 @@
+let arr = ["H"; "e"; "l"; "l"; "o"; ","; "W"; "o"; "r"; "l"; "d"; "!"];;
+let f elem =
+    Printf.printf "%s" elem in
+    List.iter f arr;;
