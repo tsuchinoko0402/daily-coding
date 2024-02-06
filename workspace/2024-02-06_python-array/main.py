690d56a1c7861edab5eece2e6710413e2aa77b19
OKAZAKI Shogo <okazaki@zakioka.net>
2024-02-07 00:56:32 +0900

    2024-02-06 Python による配列を用いた Hello, World!

---
+++ workspace/2024-02-06_python-array/main.py
@@ -0,0 +1,9 @@
+string_array = ["H", "e", "l", "l", "o", ",", "W", "o", "r", "l", "d", "!"]
+
+def main():
+    for s in string_array:
+        print(s, end = "")
+
+
+if __name__ == '__main__':
+    main()
